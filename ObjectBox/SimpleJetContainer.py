d266766828687f77ecf74c69a8f1411e2e27cb99
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2017-09-26 09:31:15 +0200

    Sync to trigger dev repo

--- ObjectBox/SimpleJetContainer.py
+++ ObjectBox/SimpleJetContainer.py
@@ -2,11 +2,17 @@ class jets:
     """
     Simple event container for jets
     """
-    def __init__(self):
+    def __init__(self, ptcut = 25, etacut = 2.5, CSVcut = 0.84, HTminpt = 30):
+        self.ptcut = ptcut
+        self.etacut = etacut
+        self.CSVcut = CSVcut
+        self.HTminpt = HTminpt
+
         self.jets = []
         self.bjets = []
         self.nJets = 0
         self.nJetsCUT = 0
+        self.nJets40 = 0
         self.nCSVM = 0
         self.HT = 0
 
@@ -15,21 +21,29 @@ class jets:
         self.bjets = []
         self.nJets = 0
         self.nJetsCUT = 0
+        self.nJets40 = 0
         self.nCSVM = 0
         self.HT = 0
 
     def add(self, pt, eta, phi, csv):
         #print "adding jet "+str(self.nJets)+"_",pt, eta, phi, csv
-        self.jets.append(jet(pt, eta, phi, csv))
-        if pt > 30 and abs(eta) < 2.4:
-            self.HT += pt
-        if pt > 30 and abs(eta) < 2.4:
-            self.nJetsCUT += 1
-        if pt > 30 and csv > 0.84 and abs(eta) < 2.4:
-            self.bjets.append(jet(pt, eta, phi, csv))
-            self.nCSVM += 1
-        self.nJets += 1
+        if pt > self.ptcut and abs(eta) < self.etacut:
+            self.jets.append(jet(pt, eta, phi, csv))
+            if pt > self.HTminpt:
+                self.HT += pt
+                self.nJetsCUT += 1
+            if pt > 40:
+                self.nJets40 += 1
+            if csv > self.CSVcut:
+                self.bjets.append(jet(pt, eta, phi, csv))
+                self.nCSVM += 1
+            self.nJets += 1
 
+    def HT4Jets(self):
+        if self.nJetsCUT >= 4:
+            return self.HT
+        else:
+            return 0
 
     def Print(self):
         print "============"
@@ -84,6 +98,7 @@ class jets:
                 if j.pt > ptmax:
                     currentjet = j
                     currentjetindex = ijet
+                    ptmax = j.pt
             retlist.append(currentjet)
             cjets.pop(currentjetindex)
         return retlist
@@ -99,3 +114,27 @@ class jet:
         self.csv = csv
     def Print(self):
         print "Jet pt: {0}, eta: {1}, phi: {2}, csv {3}".format(self.pt, self.eta ,self.phi ,self.csv)
+
+
+
+
+if __name__ == "__main__":
+    offlineJets = jets()
+
+    offlineJets.add(100, 1.0, 1.0, 0.7)
+    offlineJets.add(80, 1.28118562698, 1.0, 0.9)
+    offlineJets.add(84, -0.697551846504, 1.0, 0.88)
+    offlineJets.add(61, 1.0, 1.0, 0.3)
+    offlineJets.add(35, 1.0, 1.0, 0.45)
+    offlineJets.add(25, 1.0, 1.0, 0.6)
+
+    offlineJets.Print()
+
+    print "Jets ordered by CSV value"
+    csvorderedjets = offlineJets.getCSVorderd()
+    for jet in csvorderedjets:
+        jet.Print()
+    print "Jets ordered by pT value"
+    ptoderedjets = offlineJets.getPTorderd()
+    for jet in ptoderedjets:
+        jet.Print()
