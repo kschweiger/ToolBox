842baf29d8a1b8d2a2f407541278c697b997ee37
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2017-10-04 12:04:59 +0200

    Updates and bugfixes

--- BatchBox/BatchUtils.py
+++ BatchBox/BatchUtils.py
@@ -50,7 +50,7 @@ def create_script(name, cmsswbase, scriptpath, execString):
     #script+='export X509_USER_PROXY=/afs/cern.ch/user/k/koschwei/x509up_u88606\n'
     script+='export X509_USER_PROXY=/mnt/t3nfs01/data01/shome/koschwei/.x509up_u649\n'
     script+= execString+"\n"
-    filename=scriptpath+'/'+outfilename+'.sh'
+    filename=scriptpath+'/'+outfilename
     f=open(filename,'w')
     f.write(script)
     f.close()
--- BatchBox/ScriptHandling.py
+++ BatchBox/ScriptHandling.py
@@ -3,6 +3,9 @@ import BatchUtils
 class Sciptlist:
     def __init__(self, cmsswpath, basepath, batchtype, scriptpath = "scripts/", logpath = "logs/", errorpath = "logs/", outputpath = "output/"):
         self.scripts = []
+        self.scriptsDict = {}
+        self.scriptnames = []
+
         self.basepath = basepath
         self.system = batchtype
 
@@ -19,22 +22,40 @@ class Sciptlist:
                        "err" : self.errorpath,
                        "output" : self.outputpath }
 
-    def addScript(self, jobname , executionstring, outputname ):
-        self.scripts.append(Script(self.cmsswpath, jobname, self.paths, executionstring, outputname))
+        self.successKeys = []
+        self.failKeys = []
+
+    def addScript(self, jobname , executionstring, outputname, fromJSON = False ):
+        thisScript = Script(self.cmsswpath, jobname, self.paths, executionstring, outputname, fromJSON)
+        self.scripts.append(thisScript)
+        self.scriptsDict[jobname] = thisScript
+        self.scriptnames.append(jobname)
+
+    def addSuccessKey(self, key):
+        self.successKeys.append(key)
+
+    def addFailKey(self, key):
+        self.failKeys.append(key)
 
     def submission(self):
         for script in self.scripts:
             script.submit(self.system)
 
-    def saveList(self, savePrefix):
+    def saveList(self, savePrefix, fullName = None):
         import json
         import time
         lt = time.localtime()
 
-        filename = "{0}_{1}{2:02d}{3:02d}-{4:02d}{5:02d}{6:02d}.txt".format(savePrefix, lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec)
+        if fullName is None:
+            filename = "{0}_{1}{2:02d}{3:02d}-{4:02d}{5:02d}{6:02d}.json".format(savePrefix, lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec)
+        else:
+            filename = fullName
 
         dictForJSON = {"General" : { "basepath" : self.basepath ,
                                      "batchsystem" : self.system,
+                                     "paths" : self.paths,
+                                     "successKeys" : self.successKeys,
+                                     "failedKeys" : self.failKeys,
                                      "cmssw" : self.cmsswpath }
                       }
         for script in self.scripts:
@@ -42,8 +63,12 @@ class Sciptlist:
 
         with open(filename, 'w') as f:
             f.write(json.dumps(dictForJSON, indent=4))
+
+    def getScriptsByName(self, name):
+        if name in self.scriptnames:
+            return self.scriptsDict[name]
 class Script:
-    def __init__(self, cmssw, name, paths, execstring, output):
+    def __init__(self, cmssw, name, paths, execstring, output, fromJSON, isSubmitted = False):
         import time
         lt = time.localtime()
 
@@ -60,37 +85,79 @@ class Script:
 
         self.errfilename = "joberr_{0}".format(name)
         self.logfilename = "joblog_{0}".format(name)
-        self.scriptpath = "script_{0}".format(name)
+        self.scriptname = "{0}.sh".format(name)
         self.outfilename = output
 
         self.creationtime = "{0}{1}{2:02d}{3:02d}-{4:02d}{5:02d}{6:02d}".format("",lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec)
 
         self.batchID = None
 
-        self.isSubmitted = False
+        self.isSubmitted = isSubmitted
 
         self.executionstring = execstring
+        if not fromJSON:
+            BatchUtils.create_script(self.scriptname, self.cmsswpath, self.scriptpath, self.executionstring)
 
-        BatchUtils.create_script(self.name, self.cmsswpath, self.base, self.executionstring)
 
     def getDict(self):
         retdict = {"CreationTime" : self.creationtime,
                    "logfile" : self.logpath+"/"+self.logfilename,
                    "errfile" : self.errpath+"/"+self.errfilename,
                    "outfile" : self.outpath+"/"+self.outfilename,
+                   "scriptfile" : self.scriptpath+"/"+self.scriptname,
                    "batchID" : self.batchID,
+                   "submitted" : self.isSubmitted,
                    "execstring" : self.executionstring}
-        return self.name, retdict
+        return retdict
+
+    def printScriptInfo(self):
+        print "Printing information for script:",self.name
+        print "CreationTime",self.creationtime
+        print "logfile", self.logpath+"/"+self.logfilename
+        print "errfile", self.errpath+"/"+self.errfilename
+        print "outfile", self.outpath+"/"+self.outfilename
+        print "scriptfile", self.scriptpath+"/"+self.scriptname
+        print "batchID", self.batchID
+        #print "execstring", self.executionstring
 
     def submit(self, batchsystem):
-        if self.scriptcreated:
+        import os
+        print self.scriptpath+"/"+self.scriptname
+        if os.path.isfile(self.scriptpath+"/"+self.scriptname):
             import subprocess
             if batchsystem == "q":
-                output = subprocess.check_output("qsub -l h_vmem=4g -o {0}/{1} -e {2}/{3} {4}".format(self.logpath, self.logfilename,
-                                                                                                      self.errpath, self.errfilename,
-                                                                                                      self.scriptpath),
+                output = subprocess.check_output("qsub -l h_vmem=4g -o {0}/{1} -e {2}/{3} {4}/{5}".format(self.logpath, self.logfilename,
+                                                                                                          self.errpath, self.errfilename,
+                                                                                                          self.scriptpath, self.scriptname),
                                                  shell=True)
                 self.batchID = output.split(" ")[2]
                 self.isSubmitted = True
         else:
             print "Script not yet created"
+
+
+
+def initScirptListfromJSON(jsonfile):
+    import json
+
+    with open(jsonfile, 'r') as f:
+        readJSON = json.load(f)
+
+    #print readJSON
+
+    scripts = Sciptlist(readJSON["General"]["cmssw"], readJSON["General"]["basepath"], readJSON["General"]["batchsystem"])
+
+    for key in readJSON["General"]["successKeys"]:
+        scripts.addSuccessKey(key)
+    for key in readJSON["General"]["failedKeys"]:
+        scripts.addFailKey(key)
+
+
+    for key in readJSON:
+        if key != "General":
+            #print readJSON[key]["outfile"]
+            scripts.addScript(key, readJSON[key]["execstring"], readJSON[key]["outfile"].split("/")[-1], True)
+            scripts.getScriptsByName(key).isSubmitted = readJSON[key]["submitted"]
+            scripts.getScriptsByName(key).batchID = readJSON[key]["batchID"]
+
+    return scripts
