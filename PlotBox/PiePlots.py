ae234437cb2462ca0e4642625e7042f300131171
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2019-01-30 17:22:42 +0100

    Updated to 2017

--- PlotBox/PiePlots.py
+++ PlotBox/PiePlots.py
@@ -32,6 +32,98 @@ def getYieldsfromDataCard(filename):
 
     return yieldsection
 
+
+def getYieldsfromMLFit(filename, postfit = True, ttHPrefit = True):
+    rfile = ROOT.TFile.Open(filename)
+
+    MLFitChannels = {"j7t3" : "ch4",
+                     "j7t4" : "ch1",
+                     "j8t3" : "ch5",
+                     "j8t4" : "ch2",
+                     "j9t3" : "ch6",
+                     "j9t4" : "ch3"}
+    yields = {"j7t3" : {},
+              "j7t4" : {},
+              "j8t3" : {},
+              "j8t4" : {},
+              "j9t3" : {},
+              "j9t4" : {}}
+
+
+    if postfit:
+        topfolder = "shapes_fit_s"
+    else:
+        topfolder = "shapes_prefit"
+
+    #GetProcesses:
+    processes = []
+    for process in rfile.GetDirectory(topfolder+"/ch1/").GetListOfKeys():
+        processes.append(process.GetName())
+    print processes
+
+    for cat in yields:
+        for process in processes:
+            if not process.startswith("total"):
+                yields[cat][process] = rfile.Get(topfolder+"/"+MLFitChannels[cat]+"/"+process).Integral()
+                print cat, MLFitChannels[cat], process, yields[cat][process]
+
+    if ttHPrefit:
+        topfolder = "shapes_prefit"
+        for cat in yields:
+            for process in processes:
+                if process.startswith("ttH"):
+                    print "INFO: Setting {0} to prefit value".format(process)
+                    yields[cat][process] = rfile.Get(topfolder+"/"+MLFitChannels[cat]+"/"+process).Integral()
+                    print cat, MLFitChannels[cat], process, yields[cat][process]
+
+    return yields
+
+
+def getYieldsfromFitdiagnostics(filename, postfit = True, ttHPrefit = True):
+    rfile = ROOT.TFile.Open(filename)
+
+    MLFitChannels = {"j7t3" : "ttH_hbb_13TeV_2017_fh_7j3b_MEM",
+                     "j7t4" : "ttH_hbb_13TeV_2017_fh_7j4b_MEM",
+                     "j8t3" : "ttH_hbb_13TeV_2017_fh_8j3b_MEM",
+                     "j8t4" : "ttH_hbb_13TeV_2017_fh_8j4b_MEM",
+                     "j9t3" : "ttH_hbb_13TeV_2017_fh_9j3b_MEM",
+                     "j9t4" : "ttH_hbb_13TeV_2017_fh_9j4b_MEM"}
+    yields = {"j7t3" : {},
+              "j7t4" : {},
+              "j8t3" : {},
+              "j8t4" : {},
+              "j9t3" : {},
+              "j9t4" : {}}
+
+
+    if postfit:
+        topfolder = "shapes_fit_s"
+    else:
+        topfolder = "shapes_prefit"
+
+    #GetProcesses:
+    processes = []
+    for process in rfile.GetDirectory(topfolder+"/ch1/").GetListOfKeys():
+        processes.append(process.GetName())
+    print processes
+
+    for cat in yields:
+        for process in processes:
+            if not process.startswith("total"):
+                yields[cat][process] = rfile.Get(topfolder+"/"+MLFitChannels[cat]+"/"+process).Integral()
+                print cat, MLFitChannels[cat], process, yields[cat][process]
+
+    if ttHPrefit:
+        topfolder = "shapes_prefit"
+        for cat in yields:
+            for process in processes:
+                if process.startswith("ttH"):
+                    print "INFO: Setting {0} to prefit value".format(process)
+                    yields[cat][process] = rfile.Get(topfolder+"/"+MLFitChannels[cat]+"/"+process).Integral()
+                    print cat, MLFitChannels[cat], process, yields[cat][process]
+
+    return yields
+
 def getYieldsfromSparseHistos(filename):
     domonitoring = False
     rfile = ROOT.TFile.Open(filename)
@@ -103,7 +195,7 @@ def getYieldsfromSparseHistos(filename):
 
 def formatForPieChart(yields):
     singleslices = ['ddQCD', 'ttbarOther', 'ttbarPlusCCbar', 'ttbarPlusB', 'ttbarPlus2B',  'ttbarPlusBBbar']
-    signalprocesses = ['ttH_hbb' , 'ttH_nonhbb', "ttH_hcc", "ttH_hww", "ttH_hzz", "ttH_htt", "ttH_hgg", "ttH_hgluglu", "ttH_hzg"]
+    signalprocesses = ['ttH_hbb', "ttH_hcc", "ttH_hww", "ttH_hzz", "ttH_htt", "ttH_hgg", "ttH_hgluglu", "ttH_hzg"]
     danielColors = Helper.colors
     colors = []
     for sl in singleslices:
@@ -128,7 +220,7 @@ def formatForPieChart(yields):
 
 def getPieChart(processes, yields, colors, cat):
     #Style stuff
-    nicenames = ["Multijet","t#bar{t}+lf", "t#bar{t}+c#bar{c}", "t#bar{t}+b", "t#bar{t}+2b", "t#bar{t}+b#bar{b}", "t#bar{t}H", "Other Bkg"]
+    nicenames = ["Multijet","t#bar{t}+lf", "t#bar{t}+c#bar{c}", "t#bar{t}+b", "t#bar{t}+2b", "t#bar{t}+b#bar{b}", "t#bar{t}H (#mu = 1)", "Other Bkg"]
     niceTitle = {"j7t3" : "7 jets, 3 b tags",
                  "j7t4" : "7 jets, #geq 4 b tags",
                  "j8t3" : "8 jets, 3 b tags",
@@ -177,7 +269,8 @@ def main():
     SBs = {}
 
     plotfromDataCard = False
-    plotfromSparse = True
+    plotfromSparse = False
+    plotfromMLFit = True
     if plotfromDataCard:
         charts = {"SR" : {}}
         SBs = {"SR" : {}}
@@ -223,6 +316,39 @@ def main():
                     raw_input("")
                     exit()
                 """
+
+    if plotfromMLFit:
+        pathtoMLFit = "/Users/korbinianschweiger/Code/data/ttH/mlfit/V25_lepVetoLoose_systematics_v1_7j3bHTrewt_unblind_allsys_rateParam3_50pcTTHF/"
+        files = {"SR" : "mlfitshapes_group_group_fh_sig_9_00.root"}
+
+        for key in files:
+            print "INFO: Making PieCharts for: {0}".format(key)
+            charts.update({key: {}})
+            SBs.update({key : {}})
+            allyields = getYieldsfromMLFit(pathtoMLFit+files[key], postfit = True, ttHPrefit = True)
+            for cat in allyields:
+                print  allyields[cat]
+                processes, yields, colors = formatForPieChart(allyields[cat])
+                #print cat,"\n", processes,"\n", yields,"\n"
+                charts[key][cat], leg = getPieChart(processes, yields, colors, cat)
+                SBs[key][cat] = getSoverB(processes, yields)
+                
+    if plotfromFitDiagnostics:
+        pathtoFitDiagnostics = "/Users/korbinianschweiger/Code/data/ttH/fitdiagnostics/sparsev5_final_v1_blind_V1/"
+        files = {"SR" : "fitDiagnostics_2017_fh.roota"}
+
+        for key in files:
+            print "INFO: Making PieCharts for: {0}".format(key)
+            charts.update({key: {}})
+            SBs.update({key : {}})
+            allyields = getYieldsfromMLFit(pathtoFitDiagnostics+files[key], postfit = False, ttHPrefit = True)
+            for cat in allyields:
+                print  allyields[cat]
+                processes, yields, colors = formatForPieChart(allyields[cat])
+                #print cat,"\n", processes,"\n", yields,"\n"
+                charts[key][cat], leg = getPieChart(processes, yields, colors, cat)
+                SBs[key][cat] = getSoverB(processes, yields)
+
 
     #print charts
     niceTitle = {"j7t3" : "7 jets, 3 b tags",
@@ -288,7 +414,8 @@ def main():
         testchart.Draw("")
         """
         c1.cd(0)
-        cms = ROOT.TLatex( 0.03, 0.935, '#scale[1.2]{#scale[1.4]{#bf{CMS}} #it{Supplementary}}')
+        cms = ROOT.TLatex( 0.03, 0.935, '#scale[1.2]{#scale[1.4]{#bf{CMS}} #it{Preliminary}}')
+        #cms = ROOT.TLatex( 0.03, 0.935, '#scale[1.2]{#scale[1.4]{#bf{CMS}} #it{Supplementary}}')
         cms.SetTextFont(42)
         cms.Draw("same")
 
@@ -309,7 +436,7 @@ def main():
 
 
         c1.Update()
-        c1.Print("Pie_AllCat_{0}.pdf".format(key))
+        c1.Print("Pie_AllCat_2017_{0}.pdf".format(key))
         raw_input("Charts for {0}. Press ret for next.".format(key))
         c1.Close()
 
