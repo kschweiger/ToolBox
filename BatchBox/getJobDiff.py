7531ec484f3185bcde56076eebd0a3ce6aaec22b
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2019-11-26 16:52:09 +0100

    Added option for running from the crab status --long output instead of CSV

--- BatchBox/getJobDiff.py
+++ BatchBox/getJobDiff.py
@@ -1,8 +1,7 @@
 import csv
 from glob import glob
 
-def getJobDiff(csvFile, SEPath, delim=";"):
-    # First extract the needed information from the exported Grafana csv file
+def parseCSV(csvFile, delim):
     jobInfo ={}
     indices={"Id" : -99, "ExitCode" : -99, "Status" : -99}
     with open(csvFile, 'rb') as f_csv:
@@ -24,11 +23,51 @@ def getJobDiff(csvFile, SEPath, delim=";"):
             except StopIteration:
                 keepProcessing = False
 
+    return jobInfo
+
+def parseCrabLog(crabLog):
+    statusLines = []
+    jobInfo ={}
+    with open(crabLog, "r") as f:
+        lines = f.readlines()
+        captureLines = False
+        for line in lines:
+            if "Extended Job Status Table:" in line:
+                captureLines = True
+                continue
+            if "Summary of run jobs" in line and captureLines:
+                break
+            if captureLines:
+                _line = [l for l in line.split(" ") if l != ""]
+                _line[-1] = _line[-1].replace("\n","")
+                if len(_line) > 1:
+                    statusLines.append(_line)
+
+    jobStatus = statusLines[1::]
+    for line in jobStatus:
+
+        ID = int(line[0])
+        ExitCode = int(line[9])
+        Status = line[1]
+        jobInfo[ID] = {"Status" : Status, "ExitCode" : ExitCode}
+        
+    return jobInfo
+
+
+def getJobDiff(inFile, SEPath, isCrabLog, delim=";"):
+    # First extract the needed information from the exported Grafana csv file
+    if not isCrabLog:
+        jobInfo = parseCSV(inFile, delim)
+    else:
+        jobInfo = parseCrabLog(inFile)
+    
     #Find finished and postprocessing jobs:
     finishedJobIDs = [_id for _id in jobInfo.keys() if jobInfo[_id]["Status"] == "finished"]
+    runningJobIDs = [_id for _id in jobInfo.keys() if jobInfo[_id]["Status"].lower() == "running"]
     postprocessinJobIDs = [_id for _id in jobInfo.keys() if jobInfo[_id]["Status"] == "postProc" and jobInfo[_id]["ExitCode"] == 0]
     exitCodeGoodIDs = [_id for _id in jobInfo.keys() if jobInfo[_id]["ExitCode"] == 0]
     
+    print "Found %s jobs in Running"%(len(runningJobIDs))
     print "Found %s jobs in finished"%(len(finishedJobIDs))
     print "Found %s jobs in postProc"%(len(postprocessinJobIDs))
     print "  Sum = %s"%(len(finishedJobIDs)+len(postprocessinJobIDs))
@@ -55,15 +94,17 @@ def getJobDiff(csvFile, SEPath, delim=";"):
 
     print "Files not in CSV but on SE"
     print "  - ",onSEbutNotInCSV
+    for onSEId in onSEbutNotInCSV:
+        print "     Status according to CSV for %s is %s"%(onSEId, jobInfo[onSEId]["Status"])
     print "Files not on SE but in CSV"
     print "  - ",inCSVbutNotOnSE
-    
-        
+            
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Compare files on SE with output expected from exported Grafana stats')
     parser.add_argument('--input', action="store", required=True, help=".csv file exported from Grafana", type=str)
     parser.add_argument("--SEPath", action="store", required=True, help="Path the SE output directory. Please pass the path up to the timestemp folder **XXYYZZ_AABBCC**", type=str)
+    parser.add_argument("--isCrabLog", action="store_true", help="If passed the input file will be interpreted as log file from crab status --long")
     args = parser.parse_args()
 
-    getJobDiff(args.input, args.SEPath)
+    getJobDiff(args.input, args.SEPath, args.isCrabLog)
